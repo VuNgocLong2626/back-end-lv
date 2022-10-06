168d0510eeea7f4fbf6785afd3d09d1785b4348e
Vu_Long <long.speed00@gmail.com>
2022-10-06 14:56:43 +0700

    add config

---
+++ app/core/config.py
@@ -0,0 +1,38 @@
+import os
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    app_name: str = "TroVol Backend"
+    title: str = "TroVol example application"
+    api_prefix: str = "/trovol-backend"
+    debug: bool = True
+
+    class Config:
+        env_file = ".venv/my-env/"
+
+
+class DevSettings(Settings):
+    region_name = 'ap-southeast-1'
+    dynamo_link = 'devdynamo'
+    buckets = 'trovol-question'
+    table = 'TroVol'
+
+
+class StagingSettings(Settings):
+    region_name = 'ap-southeast-1'
+    dynamo_link = 'stagingdynamo'
+    buckets = 'trovol-question'
+    table = 'TroVol-Stagting'
+
+
+def get_settings():
+    env = os.environ.get('env')
+    if env == 'dev':
+        return DevSettings()
+    elif env == 'staging':
+        return StagingSettings()
+    return DevSettings()
+
+
+config = get_settings()
